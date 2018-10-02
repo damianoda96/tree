930cce6751136960c94bc06cd7ec8d6c64b8b026
Deven Damiano <damiano.da96@gmail.com>
2018-10-02 18:14:38 -0400

    added example

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-#Project 1 for AI - Deven Damiano - dad152@zips.uakron.edu - Nicholas Horvath - nch16@zips.uakron.edu    
+#Project 1 for AI - Deven Damiano - dad152@zips.uakron.edu - Nicholas Horvath - nch16@zips.uakron.edu
 
 import sys
+import numpy as np
+import pandas as pd
+import graphviz
 from sklearn import tree
+from sklearn import datasets
+from sklearn import svm
+from sklearn.externals import joblib
+from sklearn.model_selection import train_test_split
+
 
 #X = [[0, 0], [1, 1]]
 #Y = [0, 1]
@@ -10,8 +18,84 @@ from sklearn import tree
 #clf.predict([[2., 2.]])
 
 def learn_tree():
+    
+    #Here is an example based off of a small dataset
+    
+    features = np.array([
+        [29,23,72],
+        [31,25,77],
+        [31,27,82],
+        [29,29,89],
+        [31,31,72],
+        [29,33,77],
+    ])
+   
+    labels = np.array([
+        [0],
+        [1],
+        [1],
+        [0],
+        [1],
+        [0],
+    ])
 
-    print("Learning a decision tree and saving the tree")
+    X_train, X_test, y_train, y_test = train_test_split(
+        features,
+        labels,
+        test_size=0.3,
+        random_state=42,
+    )
+
+    clf = tree.DecisionTreeClassifier()
+    clf.fit(X=X_train,y=y_train)
+    clf.feature_importances_ # [1., 0., 0.]
+    clf.score(X=X_test, y=y_test) #1.0
+
+
+    dot_data = tree.export_graphviz(
+
+        clf,
+        out_file=None,
+        feature_names=["minutes", "age", "height"],
+        class_names=["score_equal_to_or_over_20", "scored_under_20"],
+        filled=True,
+        rounded=True,
+        special_characters=True
+    )
+
+    graph = graphviz.Source(dot_data)
+
+    graph.render('tree.gv', view=True)
+
+
+
+
+
+    #print(digits.data)
+    #print(digits.target)
+
+
+                
+
+    #print("Learning a decision tree and saving the tree")
+    
+    #while(True):
+
+    #    fileName = input("Please enter file names of attributes and training examples, press q to quit: ")
+
+    #   if(input == "q"):
+        
+        #        break
+
+        #   else:
+
+            #open file here
+
+#     continue
+
+
+
+
 
 def test_accuracy():
 
